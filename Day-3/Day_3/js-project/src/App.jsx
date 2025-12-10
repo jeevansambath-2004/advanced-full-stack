d3706a7330aba5de3122e778feee34798b3b8dbd
MithunMurugesan <s99577939@gmail.com>
2025-12-10 22:10:30 +0530

    Add Day-3 project and other files

---
+++ Day-3/Day_3/js-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Products from './Products'
+
+function App() {
+ 
+  return (
+    <>
+      <Products />
+    </>
+  )
+}
+
+export default App
